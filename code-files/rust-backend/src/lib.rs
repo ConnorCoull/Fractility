b1ffbd3bf864b03b4cdee5545851313dd83eae39
Connor <2470724c@student.gla.ac.uk>
2023-11-14 10:31:28 +0000

    refactoring

--- code-files/rust-backend/src/lib.rs
+++ code-files/rust-backend/src/lib.rs
@@ -1,19 +1,14 @@
 use wasm_bindgen::prelude::*;
 use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement};
 
-#[wasm_bindgen]
-pub fn draw_dot(canvas: &HtmlCanvasElement, x: f64, y: f64) {
-    let context = canvas
-        .get_context("2d")
-        .unwrap()
-        .unwrap()
-        .dyn_into::<CanvasRenderingContext2d>()
-        .unwrap();
+// #[wasm_bindgen]
+// pub fn draw_dot(canvas: &HtmlCanvasElement, x: f64, y: f64) {
+//     let context = get_canvas_context(&canvas);
 
-    context.begin_path();
-    context.arc(x, y, 5.0, 0.0, 2.0 * std::f64::consts::PI).unwrap();
-    context.fill();
-}
+//     context.begin_path();
+//     context.arc(x, y, 5.0, 0.0, 2.0 * std::f64::consts::PI).unwrap();
+//     context.fill();
+// }
 
 #[wasm_bindgen]
 pub fn draw_fractal(x: f64, y: f64, length: f64, length_scalar: f64, angle: f64, angle_scalar: f64, iterations: u32, canvas: &HtmlCanvasElement, thickness: f64, thickness_scalar: f64) {
@@ -28,20 +23,15 @@ pub fn draw_fractal(x: f64, y: f64, length: f64, length_scalar: f64, angle: f64,
 
     draw_line(&canvas, x, y, x2, y2, thickness);
 
-    draw_fractal(x2, y2, length * length_scalar, length_scalar, -angle*angle_scalar, angle_scalar, iterations - 1, &canvas, thickness*thickness_scalar, thickness_scalar);
-    draw_fractal(x2, y2, length * length_scalar, length_scalar, angle*angle_scalar, angle_scalar, iterations - 1, &canvas, thickness*thickness_scalar, thickness_scalar);
+    draw_fractal(x2, y2, length * length_scalar, length_scalar, -angle*angle_scalar % 360.00, angle_scalar, iterations - 1, &canvas, thickness*thickness_scalar, thickness_scalar);
+    draw_fractal(x2, y2, length * length_scalar, length_scalar, angle*angle_scalar % 360.00, angle_scalar, iterations - 1, &canvas, thickness*thickness_scalar, thickness_scalar);
 }
 
 #[wasm_bindgen]
 pub fn draw_line(canvas: &HtmlCanvasElement, x: f64, y: f64, x2: f64, y2: f64, thickness: f64) {
     // Convert the inputs to coordinates for the line
     // Then use WebGPU to draw the line on the canvas
-    let context = canvas
-        .get_context("2d")
-        .unwrap()
-        .unwrap()
-        .dyn_into::<web_sys::CanvasRenderingContext2d>()
-        .unwrap();
+    let context = get_canvas_context(&canvas);
 
     context.begin_path();
     context.move_to(x, y);
@@ -51,12 +41,34 @@ pub fn draw_line(canvas: &HtmlCanvasElement, x: f64, y: f64, x2: f64, y2: f64, t
     context.stroke();
 }
 
+// #[wasm_bindgen]
+// pub fn draw_line_given_one_point(canvas: &HtmlCanvasElement, x: f64, y: f64, angle: f64, length: f64) {
+//     let angle_radians = angle * std::f64::consts::PI / 180.00;
+
+//     let x2 = x + length * angle_radians.cos();
+//     let y2 = y + length * angle_radians.sin();
+
+//     draw_line(&canvas, x, y, x2, y2, 1.0);
+// }
+
 #[wasm_bindgen]
-pub fn draw_line_given_one_point(canvas: &HtmlCanvasElement, x: f64, y: f64, angle: f64, length: f64) {
-    let angle_radians = angle * std::f64::consts::PI / 180.00;
+pub fn clear(canvas: &HtmlCanvasElement) {
+    let context = get_canvas_context(&canvas);
 
-    let x2 = x + length * angle_radians.cos();
-    let y2 = y + length * angle_radians.sin();
+    let width = canvas.width() as f64;
+    let height = canvas.height() as f64;
+    context.clear_rect(0.0, 0.0, width, height);
+    context.clear_rect(0.0, 0.0, -width, -height);
+    context.clear_rect(0.0, 0.0, -width, height);
+    context.clear_rect(0.0, 0.0, width, -height);
+}
 
-    draw_line(&canvas, x, y, x2, y2, 1.0);
+#[wasm_bindgen]
+pub fn get_canvas_context(canvas: &HtmlCanvasElement) -> CanvasRenderingContext2d {
+    canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap()
 }
--- code-files/src/components/canvasComponent.vue
+++ code-files/src/components/canvasComponent.vue
@@ -24,12 +24,12 @@
             <p id="thickness_scalar"></p>
         </div>
         <div class="canvas-container">
-            <canvas ref="canvas" width="500" height="500"></canvas>
+            <canvas ref="canvas"></canvas>
         </div>
     </div>
 </template>
 <script>
-    import init, { draw_dot, draw_fractal, draw_line_given_one_point } from '../../public/pkg/rust_backend.js';
+    import init, { /*draw_dot,*/ draw_fractal, clear /*,draw_line_given_one_point*/ } from '../../public/pkg/rust_backend.js';
 
     export default {
         data() {
@@ -44,30 +44,26 @@
             };
         },
         methods: {
-            drawdot() {
-                const canvas = this.$refs.canvas;
-                draw_dot(canvas, this.x, this.y);
-            },
-            drawLine() {
-                const canvas = this.$refs.canvas;
-                const context = canvas.getContext('2d');
-                context.clearRect(0, 0, canvas.width, canvas.height);
-                draw_line_given_one_point(canvas, 0, 0, 0, canvas.height, 1);
-            },
+            // drawdot() {
+            //     const canvas = this.$refs.canvas;
+            //     draw_dot(canvas, this.x, this.y);
+            // },
+            // drawLine() {
+            //     const canvas = this.$refs.canvas;
+            //     clear(canvas);
+            //     draw_line_given_one_point(canvas, 0, 0, 0, canvas.height, 1);
+            // },
             drawFractal() {
-                if (this.iterations > 0) {
-                    const canvas = this.$refs.canvas;
-                    draw_fractal(0, 0, this.length, this.length_scalar, this.angle, this.angle_scalar, this.iterations, canvas, this.thickness, this.thickness_scalar);
-                    draw_fractal(0, 0, this.length, this.length_scalar, -this.angle, this.angle_scalar, this.iterations, canvas, this.thickness, this.thickness_scalar);
-                }
+                const canvas = this.$refs.canvas;
+                draw_fractal(10, 0, this.length, this.length_scalar, this.angle, this.angle_scalar, this.iterations, canvas, this.thickness, this.thickness_scalar);
+                draw_fractal(10, 0, this.length, this.length_scalar, -this.angle, this.angle_scalar, this.iterations, canvas, this.thickness, this.thickness_scalar);
             },
             clearCanvas() {
                 const canvas = this.$refs.canvas;
-                const context = canvas.getContext('2d');
-                context.clearRect(0, 0, canvas.width, canvas.height);
-                context.clearRect(0, 0, -canvas.width, -canvas.height);
-                context.clearRect(0, 0, -canvas.width, canvas.height);
-                context.clearRect(0, 0, canvas.width, -canvas.height);
+                clear(canvas);
+            },
+            update(element, value) {
+                element.innerHTML = value;
             }
         },
         async mounted() {
@@ -76,31 +72,18 @@
             const context = canvas.getContext('2d');
 
             context.canvas.width = window.innerWidth * 0.8;
-            context.canvas.height = window.innerHeight * 0.97;
+            context.canvas.height = window.innerHeight * 0.97; // this doesn't feel great but not sure what alternative look like
 
             context.translate(canvas.width / 2, canvas.height);
             context.rotate(-Math.PI / 2);
 
-            let iterationsText = document.getElementById("iterations");
-            iterationsText.innerHTML = this.iterations;
-
-            let angleText = document.getElementById("angle");
-            angleText.innerHTML = this.angle;
-
-            let lengthText = document.getElementById("length");
-            lengthText.innerHTML = this.length;
-
-            let length_scalarText = document.getElementById("length_scalar");
-            length_scalarText.innerHTML = this.length_scalar;
-
-            let thicknessText = document.getElementById("thickness");
-            thicknessText.innerHTML = this.thickness;
-
-            let angle_scalarText = document.getElementById("angle_scalar");
-            angle_scalarText.innerHTML = this.angle_scalar;
-
-            let thickness_scalarText = document.getElementById("thickness_scalar");
-            thickness_scalarText.innerHTML = this.thickness_scalar;
+            this.update(document.getElementById("iterations"), this.iterations);
+            this.update(document.getElementById("angle"), this.angle);
+            this.update(document.getElementById("length"), this.length);
+            this.update(document.getElementById("length_scalar"), this.length_scalar);
+            this.update(document.getElementById("thickness"), this.thickness);
+            this.update(document.getElementById("angle_scalar"), this.angle_scalar);
+            this.update(document.getElementById("thickness_scalar"), this.thickness_scalar);
 
             this.drawFractal();
             
@@ -109,44 +92,37 @@
         watch: {
             angle() {
                 this.clearCanvas();
-                let angleText = document.getElementById("angle");
-                angleText.innerHTML = this.angle;
+                this.update(document.getElementById("angle"), this.angle);
                 this.drawFractal();
             },
             iterations() {
                 this.clearCanvas();
-                let iterationsText = document.getElementById("iterations");
-                iterationsText.innerHTML = this.iterations;
+                this.update(document.getElementById("iterations"), this.iterations);
                 this.drawFractal();
             },
             length() {
                 this.clearCanvas();
-                let lengthText = document.getElementById("length");
-                lengthText.innerHTML = this.length;
+                this.update(document.getElementById("length"), this.length);
                 this.drawFractal();
             },
             length_scalar() {
                 this.clearCanvas();
-                let length_scalarText = document.getElementById("length_scalar");
-                length_scalarText.innerHTML = this.length_scalar;
+                this.update(document.getElementById("length_scalar"), this.length_scalar);
                 this.drawFractal();
             },
             thickness() {
                 this.clearCanvas();
-                let thicknessText = document.getElementById("thickness");
-                thicknessText.innerHTML = this.thickness;
+                this.update(document.getElementById("thickness"), this.thickness);
                 this.drawFractal();
             },
             angle_scalar() {
                 this.clearCanvas();
-                let angle_scalarText = document.getElementById("angle_scalar");
-                angle_scalarText.innerHTML = this.angle_scalar;
+                this.update(document.getElementById("angle_scalar"), this.angle_scalar);
                 this.drawFractal();
             },
             thickness_scalar() {
                 this.clearCanvas();
-                let thickness_scalarText = document.getElementById("thickness_scalar");
-                thickness_scalarText.innerHTML = this.thickness_scalar;
+                this.update(document.getElementById("thickness_scalar"), this.thickness_scalar);
                 this.drawFractal();
             }
         },
@@ -160,6 +136,8 @@
     }
     canvas {
         border-left: 1px solid black;
+        margin: none;
+        padding: none;
     }
     .container {
         display: flex;
