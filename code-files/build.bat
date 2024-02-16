635fa7f62da539da4a5f7c1ac9f9d2a958c5deba
Connor <2470724c@student.gla.ac.uk>
2024-02-16 13:37:16 +0000

    added deployment workflows and script

---
+++ code-files/build.bat
@@ -0,0 +1,6 @@
+cd .\rust-backend\
+wasm-pack build --target web
+cd ..
+rmdir /S /Q public\pkg
+xcopy /E /I rust-backend\pkg public\pkg\
+npm run build
--- code-files/build.sh
+++ code-files/build.sh
@@ -4,4 +4,4 @@ wasm-pack build --target web
 cd ..
 rm -rf public/pkg
 cp -r rust-backend/pkg/ public/pkg
-npm run serve
+npm run build
